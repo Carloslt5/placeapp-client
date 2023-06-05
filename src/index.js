1280315ab07fa116306e7b293f7680674efce4fc
lortes <lortes95@gmail.com>
2023-06-05 11:57:17 +0200

    no he cambiado nada , solo en el server la pass hasheada en modelo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AuthProviderWrapper } from './contexts/auth.context'
+import Footer from './components/Footer/Footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
