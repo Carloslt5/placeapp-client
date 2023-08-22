6f9e6a76a0acf4b5e4622804fc924ccd1ff8f139
Carloslt <carloslt.proyectos@gmail.com>
2023-08-22 13:03:38 +0200

    useCallBack and add apikey maps

--- src/pages/PlaceDetailsPage/PlaceDetailsPage.jsx
+++ src/pages/PlaceDetailsPage/PlaceDetailsPage.jsx
@@ -1,6 +1,6 @@
 import { Container } from 'react-bootstrap'
 import './PlaceDetailsPage.css'
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { useParams } from 'react-router-dom'
 import placesService from './../../services/places.services'
 import DetailsPlace from '../../components/DetailsPlace/DetailsPlace'
@@ -11,46 +11,48 @@ import CommentsList from '../../components/CommentsList/CommentsList'
 
 const PlaceDetailsPage = () => {
 
-    const { id } = useParams()
+  const { id } = useParams()
 
-    const [placeData, setPlaceData] = useState()
-
-    useEffect(() => {
-        loadPlaceDetails()
-    }, [])
-
-    const loadPlaceDetails = () => {
-        placesService
-            .getDetailsPlace(id)
-            .then(({ data }) => setPlaceData(data))
-            .catch(err => console.log(err))
-    }
-
-    return (
-
-        <>
-            <Container>
-                {
-                    !placeData
-                        ?
-                        <Loader md={{ offset: 3, span: 6 }} />
-                        :
-                        <>
-                            <DetailsPlace {...placeData} />
-
-                            <CommentsList placeData={placeData} updateComments={loadPlaceDetails} />
-
-                            <CreateCommentForm updateComments={loadPlaceDetails} />
-                        </>
-
-                }
-
-            </Container>
+  const [placeData, setPlaceData] = useState()
 
 
-        </>
+  const loadPlaceDetails = useCallback(() => {
+    placesService
+      .getDetailsPlace(id)
+      .then(({ data }) => setPlaceData(data))
+      .catch(err => console.log(err))
+  }, [id])
 
-    )
+  useEffect(() => {
+    loadPlaceDetails()
+  }, [loadPlaceDetails])
+
+  return (
+
+    <>
+      <Container>
+        {
+          !placeData
+            ?
+            <Loader md={{ offset: 3, span: 6 }} />
+            :
+            <>
+              <DetailsPlace {...placeData} />
+
+              <CommentsList placeData={placeData} updateComments={loadPlaceDetails} />
+
+              <CreateCommentForm updateComments={loadPlaceDetails} />
+            </>
+
+        }
+
+      </Container>
+
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAirnevs0SFUHkMNgNV7xEX49GkuwqE_XM&libraries=places" async defer></script>
+
+    </>
+
+  )
 }
 
 
