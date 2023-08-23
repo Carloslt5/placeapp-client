29506ab2c7a1d99d78014b23d52da6531b18834a
Carloslt <carloslt.proyectos@gmail.com>
2023-08-23 11:44:27 +0200

    fix apikey

--- src/services/places.services.js
+++ src/services/places.services.js
@@ -2,59 +2,59 @@ import axios from 'axios'
 
 class PlacesService {
 
-    constructor() {
+  constructor() {
 
-        this.api = axios.create({
-            baseURL: `${process.env.REACT_APP_API_URL}/places`
-        })
+    this.api = axios.create({
+      baseURL: `${process.env.REACT_APP_API_URL}/places`
+    })
 
-        this.api.interceptors.request.use((config) => {
+    this.api.interceptors.request.use((config) => {
 
-            const storedToken = localStorage.getItem("authToken")
+      const storedToken = localStorage.getItem("authToken")
 
-            if (storedToken) {
-                config.headers = { Authorization: `Bearer ${storedToken}` }
-            }
+      if (storedToken) {
+        config.headers = { Authorization: `Bearer ${storedToken}` }
+      }
 
-            return config
-        })
-    }
+      return config
+    })
+  }
 
-    getAllPlaces() {
-        return this.api.get('/getAllPlaces')
-    }
+  getAllPlaces() {
+    return this.api.get('/getAllPlaces')
+  }
 
-    getUserPlaces(user_id) {
-        return this.api.get(`/${user_id}/getUserPlaces`)
-    }
+  getUserPlaces(user_id) {
+    return this.api.get(`/${user_id}/getUserPlaces`)
+  }
 
-    getDetailsPlace(place_id) {
-        return this.api.get(`/${place_id}/getDetailsPlace`)
-    }
+  getDetailsPlace(place_id) {
+    return this.api.get(`/${place_id}/getDetailsPlace`)
+  }
 
-    createPlace(placeData) {
-        return this.api.post(`/createPlace`, placeData)
-    }
+  createPlace(placeData) {
+    return this.api.post(`/createPlace`, placeData)
+  }
 
-    getOnePlace(placeId) {
-        return this.api.get(`/getOnePlace/${placeId}`)
-    }
+  getOnePlace(placeId) {
+    return this.api.get(`/getOnePlace/${placeId}`)
+  }
 
-    editPlace(place_id, placeData) {
-        return this.api.put(`/${place_id}/edit`, placeData)
-    }
+  editPlace(place_id, placeData) {
+    return this.api.put(`/${place_id}/edit`, placeData)
+  }
 
-    addFavouritesPlace(placeIdFavourite, userId) {
-        return this.api.put(`/${placeIdFavourite}/addfavourites`, userId)
-    }
+  addFavouritesPlace(placeIdFavourite, userId) {
+    return this.api.put(`/${placeIdFavourite}/addfavourites`, userId)
+  }
 
-    removeFavouritesPlace(placeIdFavourite, userId) {
-        return this.api.put(`/${placeIdFavourite}/removefavourites`, userId)
-    }
+  removeFavouritesPlace(placeIdFavourite, userId) {
+    return this.api.put(`/${placeIdFavourite}/removefavourites`, userId)
+  }
 
-    deletePlace(placeIdDelete) {
-        return this.api.delete(`/${placeIdDelete}/delete`)
-    }
+  deletePlace(placeIdDelete) {
+    return this.api.delete(`/${placeIdDelete}/delete`)
+  }
 
 
 }
